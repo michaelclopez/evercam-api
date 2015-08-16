b856710661f2e1ef2631c9f73abc017fbcb08a2f
Eugene HP <eugene.hp2012@gmail.com>
2015-08-16 13:36:30 +0200

    fix indents

--- migrations/20150813143400_add_motion_detection_in_snapshots_table.rb
+++ migrations/20150813143400_add_motion_detection_in_snapshots_table.rb
@@ -1,16 +1,13 @@
 Sequel.migration do
-
   up do
-
     alter_table(:snapshots) do
       add_column :motiondetection, :integer, null: true
     end
-
   end
 
   down do
-    alter_table(:snapshots)
+    alter_table(:snapshots) do
       drop_column :motiondetection
+    end
   end
-
 end
