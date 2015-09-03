27ffc444892b7001d77946c525e187cfe39ecd59
Azhar Malik <azharmalik02@gmail.com>
2015-09-03 17:59:01 +0500

    Added up and down methods.

--- migrations/20150813151434_remove_is_admin_field.rb
+++ migrations/20150813151434_remove_is_admin_field.rb
@@ -1,7 +1,13 @@
 Sequel.migration do
-  change do
+  up do
     alter_table(:users) do
       drop_column :is_admin
+    end
+  end
+
+  down do
+    alter_table(:users) do
+      add_column :is_admin, :boolean
     end
   end
 end
