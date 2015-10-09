b4e82a15c877ba922ec969ceb99ad9085c06b292
Miloš Mošić <mosic.milos@gmail.com>
2015-10-09 15:37:45 +0200

    Use cam auth in token sent to evercam-media

--- lib/workers/camera_touch_worker.rb
+++ lib/workers/camera_touch_worker.rb
@@ -1,3 +1,5 @@
+require 'openssl'
+require 'base64'
 require_relative '../../lib/services'
 
 module Evercam
@@ -7,13 +9,21 @@ module Evercam
     sidekiq_options queue: :status
 
     def perform(camera_id)
+      camera = Camera.by_exid!(camera_id)
+      cam_username = camera.config.fetch('auth', {}).fetch('basic', {}).fetch('username', '')
+      cam_password = camera.config.fetch('auth', {}).fetch('basic', {}).fetch('password', '')
+      cam_auth = "#{cam_username}:#{cam_password}"
+
+      credentials = ":"
+
       cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
       cipher.encrypt
       cipher.key = "#{Evercam::Config[:snapshots][:key]}"
       cipher.iv = "#{Evercam::Config[:snapshots][:iv]}"
       cipher.padding = 0
 
-      message = "|||"
+      message = ""
+      message << "|#{cam_auth}|#{credentials}|#{Time.now.utc.iso8601}|"
       message << ' ' until message.length % 16 == 0
       token = cipher.update(message)
       token << cipher.final
