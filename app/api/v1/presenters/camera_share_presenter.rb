83f6e0d79c7e571d2e9f42ae76d99c8aeaf9dda4
azharmalik3 <azharmalik02@gmail.com>
2015-10-16 16:04:02 +0500

    hound style indentation

--- app/api/v1/presenters/camera_share_presenter.rb
+++ app/api/v1/presenters/camera_share_presenter.rb
@@ -6,73 +6,70 @@ module Evercam
       root :shares
 
       expose :id, documentation: {
-                  type: 'integer',
-                  desc: 'Unique identifier for a camera share.',
-                  required: true
-                }
+        type: 'integer',
+        desc: 'Unique identifier for a camera share.',
+        required: true
+      }
 
-      expose :camera_id,
-             documentation: {
-               type: 'string',
-               desc: 'Unique identifier of the shared camera.',
-               required: true
-             } do |s, o|
+      expose :camera_id, documentation: {
+        type: 'string',
+        desc: 'Unique identifier of the shared camera.',
+        required: true
+      } do |s, o|
         s.camera.exid
       end
 
       expose :sharer_id, documentation: {
-                         type: 'string',
-                         desc: 'The unique identifier of the user who shared the camera.',
-                         required: true
-                       } do |s,o|
+        type: 'string',
+        desc: 'The unique identifier of the user who shared the camera.',
+        required: true
+      } do |s,_o|
         s.sharer ? s.sharer.username : nil
       end
 
       expose :sharer_name, documentation: {
-                           type: 'string',
-                           desc: 'Full name of the user the camera is shared with.',
-                           required: true
-                         } do |s, o|
+        type: 'string',
+        desc: 'Full name of the user the camera is shared with.',
+        required: true
+      } do |s, _o|
         s.sharer ? s.sharer.fullname : nil
       end
 
       expose :user_id, documentation: {
-                       type: 'string',
-                       desc: 'Unique user id of the user the camera is shared with.',
-                       required: true
-                     } do |s, o|
+        type: 'string',
+        desc: 'Unique user id of the user the camera is shared with.',
+        required: true
+      } do |s, _o|
         s.user.username
       end
 
       expose :fullname, documentation: {
-                        type: 'string',
-                        desc: 'Full name of the user the camera is shared with.',
-                        required: true
-                      } do |s, o|
+        type: 'string',
+        desc: 'Full name of the user the camera is shared with.',
+        required: true
+      } do |s, _o|
         s.user.fullname
       end
 
       expose :email, documentation: {
-                     type: 'string',
-                     desc: 'Email of the user the camera is shared with.',
-                     required: true
-                   } do |s, o|
+        type: 'string',
+        desc: 'Email of the user the camera is shared with.',
+        required: true
+      } do |s, o|
         s.user.email
       end
 
-      expose :kind,
-             documentation: {
-               type: 'string',
-               desc: "Either 'public' or 'private' depending on the share kind.",
-               required: true
-             }
+      expose :kind, documentation: {
+        type: 'string',
+        desc: "Either 'public' or 'private' depending on the share kind.",
+        required: true
+      }
 
-      expose :rights,
-             documentation: {
-               type: 'string',
-               desc: "A comma separated list of the rights available on the share.",
-               required: true
-             } do |s, o|
+      expose :rights, documentation: {
+        type: 'string',
+        desc: "A comma separated list of the rights available on the share.",
+        required: true
+      } do |s, _o|
         list = []
         if s.kind == 'private'
           rights = AccessRightSet.for(s.camera, s.user)
