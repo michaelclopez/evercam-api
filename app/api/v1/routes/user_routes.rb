75a34189e3d50b4528b991755dc1ad726e71480e
azharmalik3 <azharmalik02@gmail.com>
2015-11-24 17:54:27 +0500

    Fixed broken tests

--- app/api/v1/routes/user_routes.rb
+++ app/api/v1/routes/user_routes.rb
@@ -107,7 +107,6 @@ module Evercam
         requires :id, type: String, desc: "Username"
         optional :firstname, type: String, desc: "First Name"
         optional :lastname, type: String, desc: "Last Name"
-        optional :username, type: String, desc: "Username"
         optional :country, type: String, desc: "Country"
         optional :email, type: String, desc: "Email"
         optional :billing_id, type: String, desc: "Billing ID"
@@ -118,7 +117,7 @@ module Evercam
         params[:id] = params[:id][0..-6] if params[:id].end_with?('.json')
         params[:id] = params[:id][0..-5] if params[:id].end_with?('.xml')
         params[:id].downcase!
-        params[:email].downcase!
+        params[:email].downcase! if params[:email]
         target = ::User.by_login(params[:id])
         raise NotFoundError, 'user does not exist' unless target
 
