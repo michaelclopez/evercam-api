8eee639cdcce0bd3dfb8af8253eac9996a05b734
Junaid Farooq <junaid@evercam.io>
2016-01-19 15:26:46 +0500

    Added drop

--- migrations/20160119095900_add_snapshot_count_to_snapshot_reports.rb
+++ migrations/20160119095900_add_snapshot_count_to_snapshot_reports.rb
@@ -4,4 +4,10 @@ Sequel.migration do
       add_column :snapshot_count, :integer, null: false
     end
   end
+
+  down do
+	  alter_table(:snapshot_reports) do
+	    drop_column :snapshot_count
+	  end
+	end
 end
