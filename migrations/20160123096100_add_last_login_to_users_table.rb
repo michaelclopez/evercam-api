64336f6481815c81feea1e2bfb979a3b5e6e405f
Junaid Farooq <junaid@evercam.io>
2016-01-25 17:02:55 +0500

    changed col name

--- migrations/20160123096100_add_last_login_to_users_table.rb
+++ migrations/20160123096100_add_last_login_to_users_table.rb
@@ -1,13 +1,13 @@
 Sequel.migration do
   up do
     alter_table(:users) do
-      add_column :last_login, :timestamptz, null: true
+      add_column :last_login_at, :timestamptz, null: true
     end
   end
 
   down do
     alter_table(:users) do
-      drop_column :last_login
+      drop_column :last_login_at
     end
   end
 end
