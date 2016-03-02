3835f4d29f35747d1222e7cc194fd33b9edaa828
Junaid Farooq <junaid@evercam.io>
2016-03-02 15:55:47 +0500

    Changed vat to vat_number

--- migrations/20160327114642_add_vat_to_users_table.rb
+++ migrations/20160327114642_add_vat_to_users_table.rb
@@ -1,13 +1,13 @@
 Sequel.migration do
   up do
     alter_table(:users) do
-      add_column :vat, :text, null: true
+      add_column :vat_number, :text, null: true
     end
   end
 
   down do
     alter_table(:users) do
-      drop_column :vat
+      drop_column :vat_number
     end
   end
 end
