2891cfd4c3fea4c187f6c692332fa9cf4ddba32e
Junaid Farooq <junaid@evercam.io>
2016-04-12 18:53:28 +0500

    Changed sentences and message

--- app/api/v1/routes/share_routes.rb
+++ app/api/v1/routes/share_routes.rb
@@ -233,15 +233,14 @@ module Evercam
       desc 'Resend an existing camera share request for a given camera.'
       params do
         requires :id, type: String, desc: "The unique identifier for a camera."
-        requires :email, type: String, desc: "Email address or user name of the user to share the camera with."
+        requires :email, type: String, desc: "Email address of the user to share the camera with."
       end
       post '/:id/shares/requests' do
         camera = get_cam(params[:id])
         share_request = CameraShareRequest.where(camera_id: camera.id, email: params[:email]).first
         raise NotFoundError.new if share_request.nil?
         EmailWorker.perform_async(type: 'share_request', user: caller.username, email: params[:email], sharer: caller.email, message: share_request.message, camera: camera.exid)
-        message = "A notification email has been re-sent to the specified email address."
-        { "message": message }
+        { "message": "A notification email has been re-sent to the specified email address." }
       end
 
       #-------------------------------------------------------------------
